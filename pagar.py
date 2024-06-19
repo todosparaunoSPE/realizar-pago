d7c9d34291dcaff06786f45a4c1c13b45557b959
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-06-19 11:35:00 -0600

    Add files via upload

---
+++ pagar.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 10:27:45 2024
+
+@author: jperezr
+"""
+
+import streamlit as st
+
+# Título de la Aplicación
+st.title('Pagar Servicio de Gestión de Contactos')
+
+# URL del producto en Stripe
+stripe_product_url = "https://buy.stripe.com/6oEeYqaG37WVfiE288"
+
+# Mostrar el enlace para pagar
+if st.button('Pagar'):
+    st.write("Procede al siguiente enlace para completar el pago:")
+    st.markdown(f'<a href="{stripe_product_url}" target="_blank">Pagar ahora</a>', unsafe_allow_html=True)
